e7d369b69e4bc83eaf3c1f3e6d7ca8ea376b5366
Concourse <concourse@finbourne.com>
2023-11-03 11:54:23 +0000

    Updates for v2.0.1

--- sdk/Finbourne.Workflow.Sdk/Extensions/ApiResponseExtensions.cs
+++ sdk/Finbourne.Workflow.Sdk/Extensions/ApiResponseExtensions.cs
@@ -18,7 +18,7 @@ namespace Finbourne.Workflow.Sdk.Extensions
         /// <summary>
         /// The header that the request id is contained within.
         /// </summary>
-        public const string RequestIdHeader = "lusid-meta-requestid";
+        public const string RequestIdHeader = "workflow-meta-requestid";
         
         /// <summary>
         /// The header that the Date Time Offset is contained within.
