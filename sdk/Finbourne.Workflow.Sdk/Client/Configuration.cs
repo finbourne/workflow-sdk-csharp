eadb6317b99b4ae95fb45550faf6f23ab5651f8f
Concourse <concourse@finbourne.com>
2024-10-18 14:29:52 +0000

    workflow csharp updates for v2.1.92
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/Finbourne.Workflow.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Workflow.Sdk/Client/Configuration.cs
@@ -594,7 +594,7 @@ namespace Finbourne.Workflow.Sdk.Client
             string report = "C# SDK (Finbourne.Workflow.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.1.1286\n";
+            report += "    Version of the API: 0.1.1288\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
